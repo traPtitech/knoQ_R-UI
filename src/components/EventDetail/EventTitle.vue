896b23b9fd0a11d5d092f8d36b18cbef5e922759
itt828 <iittt828@gmail.com>
2022-08-14 02:16:21 +0900

    :lipstick: fix font-size

--- src/components/EventDetail/EventTitle.vue
+++ src/components/EventDetail/EventTitle.vue
@@ -1,5 +1,11 @@
 <template>
-  <div>knoQ meeting</div>
+  <div :class="$style.title">knoQ meeting</div>
 </template>
 
 <script setup lang="ts"></script>
+
+<style lang="scss" module>
+.title {
+  font-size: 34pt;
+}
+</style>
