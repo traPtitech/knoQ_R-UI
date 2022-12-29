eaa35a136f79123956f66cee1308b24555a0cbf4
itt828 <iittt828@gmail.com>
2022-12-29 22:13:22 +0900

    :zap: fix api config

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,14 +1,26 @@
-import { Configuration, EventsApi, UsersApi } from "./generated";
+import {
+  Configuration,
+  EventsApi,
+  UsersApi,
+  AuthenticationApi,
+} from "./generated";
 
 export const BASE_PATH = "http://localhost:6006/api";
-const config = new Configuration({ basePath: BASE_PATH });
+const config = new Configuration({
+  basePath: BASE_PATH,
+  baseOptions: {
+    withCredentials: true,
+  },
+});
 
 const usersApi = new UsersApi(config, "");
 const eventsApi = new EventsApi(config, "");
+const authApi = new AuthenticationApi(config, "");
 
 const api = {
   users: usersApi,
   events: eventsApi,
+  auth: authApi,
 };
 
 export default api;
