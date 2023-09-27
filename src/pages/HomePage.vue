66c5bfbeca0500a11f5cb1c9b21e0b1501643823
itt828 <iittt828@gmail.com>
2023-09-27 15:06:03 +0900

    :sparkles: Update top page

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,33 +1,62 @@
 <template>
-  <div v-if="meError">failed to load</div>
-  <div v-if="!me">loading me</div>
-  <div v-else>hello {{ me.name }}</div>
-  <div v-if="todayEventsError">failed to load events</div>
-  <div v-if="!todayEvents">loading events</div>
-  <div v-else>{{ todayEvents.map((event) => event.name) }}</div>
-  <RouterLink to="/events">/events</RouterLink>
-  <div v-if="!data">loading events</div>
-  <div v-else>{{ data?.map((v) => v.name) }}</div>
-  <div>{{ error }}</div>
+  <div>
+    <h2>あなた</h2>
+    <div v-if="meError">failed to load</div>
+    <div v-else-if="!me">loading me</div>
+    <div v-else>{{ me.name }}</div>
+  </div>
+
+  <div>
+    <h2>今日のイベント</h2>
+    <div v-if="todaysEventsError">failed to load events</div>
+    <div v-else-if="!todaysEvents">loading events</div>
+    <div v-else>
+      <div v-for="event in todaysEvents" :key="event.eventId">
+        <RouterLink :to="`/events/${event.eventId}`"> {{ event.name }}</RouterLink>
+      </div>
+    </div>
+  </div>
+  <div>
+    <h2>あなたのイベント</h2>
+    <div v-if="myEventsError">failed to load events</div>
+    <div v-else-if="!myEvents">loading events</div>
+    <div v-else>
+      <div v-for="event in myEvents" :key="event.eventId">
+        <RouterLink :to="`/events/${event.eventId}`"> {{ event.name }}</RouterLink>
+      </div>
+    </div>
+  </div>
+  <div>
+    <h2>あなたのグループ</h2>
+    <div v-if="myGroupsError">failed to load events</div>
+    <div v-else-if="!myGroupDetails">loading events</div>
+    <div v-else>
+      <div v-for="group in myGroupDetails" :key="group?.groupId">
+        <RouterLink :to="`/groups/${group?.groupId}`"> {{ group?.name }}</RouterLink>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script setup lang="ts">
-import useSWRV from 'swrv'
 import { RouterLink } from 'vue-router'
-import { fetcher } from '../lib/fetcher'
-import { paths } from '../api/schema'
 import { useApiSWRV } from '../composables/useApiSWRV'
+import { computed } from 'vue'
 
-const { data: me, error: meError } = useSWRV<
-  paths['/users/me']['get']['responses']['200']['content']['application/json']
->('/users/me', fetcher)
-
-const { data, error } = useApiSWRV('/events', {
+const { data: me, error: meError } = useApiSWRV('/users/me', {})
+const { data: todaysEvents, error: todaysEventsError } = useApiSWRV('/events', {
   params: {
     query: { dateBegin: '2023-09-27T00:00:00+09:00', dateEnd: '2023-09-27T23:59:59+09:00' }
   }
 })
-const { data: todayEvents, error: todayEventsError } = useSWRV<
-  paths['/events']['get']['responses']['200']['content']['application/json']
->('/events', fetcher)
+const { data: myEvents, error: myEventsError } = useApiSWRV('/users/me/events', {
+  params: { query: { relation: 'attendees' } }
+})
+const { data: myGroups, error: myGroupsError } = useApiSWRV('/users/me/groups', {
+  params: { query: { relation: 'belongs' } }
+})
+const { data: allGroups, error: allGroupsError } = useApiSWRV('/groups', {})
+const myGroupDetails = computed(
+  () => myGroups.value?.map((v) => allGroups.value?.filter((x) => x.groupId === v)).flat()
+)
 </script>
