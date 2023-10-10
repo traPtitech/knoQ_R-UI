bb82d7451668e2f88e13316cecc7758afc53f372
itt828 <iittt828@gmail.com>
2023-10-10 17:43:28 +0900

    :zap: Custom match url

--- src/lib/mock.ts
+++ src/lib/mock.ts
@@ -11,6 +11,11 @@ import { PathsWithMethod } from 'openapi-typescript-helpers'
 import { paths } from '../api/schema'
 import { BASE_URL } from '../api'
 
+const mswUrlFixer = (path: string): string => {
+  const s = path.replace(/\{(.+)\}/, ':$1')
+  return BASE_URL + s
+}
+
 type Resolver = ResponseResolver<
   RestRequest<never, PathParams<string>>,
   RestContext,
@@ -18,15 +23,15 @@ type Resolver = ResponseResolver<
 >
 const _mockApi = <Paths extends {}>() => {
   const get = <P extends PathsWithMethod<Paths, 'get'>>(path: P, resolver: Resolver) =>
-    rest.get(BASE_URL + (path as string), resolver)
+    rest.get(mswUrlFixer(path as string), resolver)
   const post = <P extends PathsWithMethod<Paths, 'post'>>(path: P, resolver: Resolver) =>
-    rest.post(BASE_URL + (path as string), resolver)
+    rest.post(mswUrlFixer(path as string), resolver)
   const put = <P extends PathsWithMethod<Paths, 'put'>>(path: P, resolver: Resolver) =>
-    rest.put(BASE_URL + (path as string), resolver)
+    rest.put(mswUrlFixer(path as string), resolver)
   const patch = <P extends PathsWithMethod<Paths, 'patch'>>(path: P, resolver: Resolver) =>
-    rest.patch(BASE_URL + (path as string), resolver)
+    rest.patch(mswUrlFixer(path as string), resolver)
   const _delete = <P extends PathsWithMethod<Paths, 'delete'>>(path: P, resolver: Resolver) =>
-    rest.delete(BASE_URL + (path as string), resolver)
+    rest.delete(mswUrlFixer(path as string), resolver)
   return {
     get,
     post,
