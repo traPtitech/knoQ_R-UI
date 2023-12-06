950f09efe96f8b6a4e348a782215be13e9819f8a
itt828 <iittt828@gmail.com>
2023-12-07 02:45:15 +0900

    :wrench: update eslint config
    :wrench: Update eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,19 +4,15 @@ module.exports = {
     node: true
   },
   extends: [
-    'plugin:vue/vue3-essential',
     'eslint:recommended',
+    'plugin:vue/vue3-recommended',
     'plugin:prettier/recommended',
     'plugin:storybook/recommended'
   ],
+  parser: 'vue-eslint-parser',
   parserOptions: {
     ecmaVersion: 2020,
     parser: '@typescript-eslint/parser'
   },
-  rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-undef': 'off',
-    'no-unused-vars': 'off'
-  }
+  rules: {}
 }
